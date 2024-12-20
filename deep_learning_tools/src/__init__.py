b55fc7d928bbdd4b05e33a5dc9a729cc624d4a0b
Goose <evilgoosecode@gmail.com>
2024-12-20 16:30:11 +0100

    removed old sweep util

--- deep_learning_tools/src/__init__.py
+++ deep_learning_tools/src/__init__.py
@@ -1,7 +1,7 @@
 from .training import ModelTrainer, EarlyStopping, accuracy, precision, recall, f1_score
 from .visualization import MetricsPlotter
 from .logging import LoggerManager, BaseLogger, create_logger, NullLogger, FileLogger, TensorBoardLogger, WandBLogger
-from .utils import initialize_wandb_sweep, prepare_datasets
+from .utils import prepare_datasets
 
 __all__ = [
     'ModelTrainer',
@@ -18,6 +18,5 @@ __all__ = [
     'FileLogger',
     'TensorBoardLogger',
     'WandBLogger',
-    'initialize_wandb_sweep',
     'prepare_datasets'
 ]
