d7b423174e42093d9730de57fe675f1af1a2f20e
Goose <evilgoosecode@gmail.com>
2024-12-29 13:11:25 +0100

    testing sweep

--- deep_learning_tools/src/sweeps/sweep_config.py
+++ deep_learning_tools/src/sweeps/sweep_config.py
@@ -30,11 +30,11 @@ sweep_config: Dict[str, Any] = {
             'distribution': 'uniform'
         },
         'early_stopping_patience': {
-            'values': [5],
+            'values': [2],
             'distribution': 'categorical'
         },
         'early_stopping_delta': {
-            'values': [1e-4],
+            'values': [0.5],
             'distribution': 'categorical'
         },
         'learning_rate': {
